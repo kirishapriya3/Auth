e68949985f0e2b3710f6f05092c847942eea5e0e
kirishapriya3 <142150549+kirishapriya3@users.noreply.github.com>
2026-02-22 23:07:30 +0530

    Update README.js

--- README.js
+++ README.js
@@ -25,3 +25,18 @@ Authorization: Bearer <token>
 ## How to Run Locally
 npm install
 npm run dev
+
+# Deployed Link:
+https://auth-6dcz.onrender.com
+
+# API Routes:
+POST /api/user/register
+POST /api/user/login
+GET /api/user/getUser (Protected - Requires Token) 
+Token : eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5OWIzOWY5ZGYyZTVlYjFhYWEyODk1NyIsImVtYWlsIjoidGVzdEBnbWFpbC5jb20iLCJpYXQiOjE3NzE3ODA2NDMsImV4cCI6MTc3MTc4NDI0M30.sjZVS-FRtV9g1m0SGf5-gcbm4EEAks7oNnLRyHtq2qo
+
+# How to Test:
+1. Register
+2. Login
+3. Copy token
+4. Use token in Authorization header
